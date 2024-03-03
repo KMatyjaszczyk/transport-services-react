dadc98ea7d9f137d799922400d527f1746cc1dd6
Krzysztof Matyjaszczyk <krzysiek.matyjaszczyk@gmail.com>
2024-03-03 13:29:32 +0100

    Add dark mode dropdown

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import { toast } from "react-toastify"
 import axios from "axios"
+import DarkModeDropdown from "./DarkModeDropdown";
 
 const Navbar = ({ userIsLoggedIn, setUserIsLoggedIn }) => {
     const navigate = useNavigate()
@@ -46,6 +47,7 @@ const Navbar = ({ userIsLoggedIn, setUserIsLoggedIn }) => {
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li><DarkModeDropdown></DarkModeDropdown></li>
                     <li className="nav-item"><Link className="nav-link" to="/about">About</Link></li>
                     { userIsLoggedIn && <li className="nav-item"><Link className="nav-link" to="/reservations">Reservations</Link></li> }
                     { userIsLoggedIn ? 
